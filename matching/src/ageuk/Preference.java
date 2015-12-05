dc8401b7236460402b81d661bba0cffcd000a505
aurelien zakowic <azakowic@gmail.com>
2015-12-05 00:52:53 +0000

    /add matching

---
+++ matching/src/ageuk/Preference.java
@@ -0,0 +1,23 @@
+package ageuk;
+
+public class Preference {
+    private char gender;
+    private Couple age;
+    
+    public char getGender() {
+   	 return gender;
+    }
+    public void setGender(char gender) {
+   	 this.gender = gender;
+    }
+    public Couple getAge() {
+   	 return age;
+    }
+    public void setAge(Couple age) {
+   	 this.age = age;
+    }
+    
+    
+
+}
+
